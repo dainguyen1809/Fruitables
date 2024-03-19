4538844175d9be863ab4bc7b2d0996ecbd9e4a9c
dainguyen.nhd <dainguyen.nhd@gmail.com>
2024-03-19 13:43:00 +0700

    migration

---
+++ database/migrations/2024_03_19_061529_create_banners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('URL')->default('#');
+            $table->string('image');
+            $table->string('description');
+            $table->string('position')->default('top-banner');
+            $table->smallInteger('prioty')->default(0);
+            $table->smallInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
